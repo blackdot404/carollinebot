aabf74deca5055a504d06cae5eacf27d69318964
blackdot404 <blackstv11@gmail.com>
2022-11-26 03:31:07 -0300

    ✨ mongodb inclusion

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -1,9 +1,23 @@
-const { Client } = require('discord.js');
+require("dotenv").config();
+const { Client } = require("discord.js");
+const mongoose = require("mongoose");
+const { USER_DB, PASS_DB } = process.env;
 
 module.exports = {
-    name: 'ready',
+    name: "ready",
     once: true,
-    execute(client) {
-        console.log(`${client.user.username} is now online.`)
-    }
-}
+    async execute(client) {
+        await mongoose.connect(
+            `mongodb+srv://${USER_DB}:${PASS_DB}@cronus.dfu2wp6.mongodb.net/?retryWrites=true&w=majority` ||
+                "",
+            {
+                keepAlive: true,
+            }
+        );
+        if (mongoose.connect) {
+            console.log("Connection to NoSQL database was successful.");
+        }
+
+        console.log(`${client.user.username} is now online.`);
+    },
+};
