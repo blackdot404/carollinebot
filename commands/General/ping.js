bcd4b8b72d0570fa400dc668bf99bc19aa1ca96e
blackdot404 <blackstv11@gmail.com>
2023-05-11 14:55:59 -0300

    ♻️ reset code

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -1,20 +1,31 @@
-const { SlashCommandBuilder, CommandInteraction } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Return my ping!"),
+        .setName('ping')
+        .setDescription('Retorno a latencia atual com a API do discord.'),
 
     async execute(interaction, client) {
-        const message = await interaction.deferReply({
-            fetchReply: true,
-        });
+        // const message = await interaction.deferReply({
+        //     fetchReply: true,
+        // });
 
-        const newMessage = `API Latency: ${client.ws.ping}ms.\nClient Ping: ${
-            message.createdTimestamp - interaction.createdTimestamp
-        }ms.`;
-        await interaction.editReply({
-            content: newMessage,
+        // const newMessage = `API Latency: ${client.ws.ping}ms.\nClient Ping: ${
+        //     message.createdTimestamp - interaction.createdTimestamp
+        // }ms.`;
+
+        const newMessageEmbed = new EmbedBuilder()
+            .setColor(16312092)
+            .setTitle('Ping 🏓')
+            .setDescription(`Bot Latency: ${client.ws.ping}ms. :comet:`)
+            .setTimestamp()
+            .setFooter({
+                text: interaction.user.username,
+                iconURL: interaction.user.avatarURL(),
+            });
+
+        await interaction.reply({
+            embeds: [newMessageEmbed],
         }); // Pode ser inserido o criterio 'ephemeral: true' resposta visivel só pra quem mandou o comando.
     },
 };
