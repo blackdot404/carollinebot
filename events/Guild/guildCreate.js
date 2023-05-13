416c1f462d1fef7d08cb154cc255804f1c911bfd
blackdot404 <blackstv11@gmail.com>
2023-05-13 17:06:12 -0300

    ✨ Introduce new features

--- events/Guild/guildCreate.js
+++ events/Guild/guildCreate.js
@@ -1,4 +1,5 @@
 const Schema = require('../../Models/Welcome');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'guildCreate',
@@ -11,12 +12,39 @@ module.exports = {
             Role: null,
         });
 
-        // const guildId = client.guilds.cache.get(guild.id);
-        // console.log(guildId.systemChannelId);
-        // const channels = guildId.channels.cache.filter(
-        //     (channel) => channel.type === 'text'
-        // );
-        // console.log(channels.map((channel) => channel.id));
+        const guildId = client.guilds.cache.get(guild.id);
+        const channelGuild = guildId.channels.cache.get(
+            guildId.systemChannelId
+        );
+
+        const messageEmbedded = new EmbedBuilder()
+            .setColor(16312092)
+            .setTitle('Oiee!! :heart: :black_heart:')
+            .setDescription(
+                `Olá amigos! Cheguei no servidor, ${guild.name} para ajudar a vocês.`
+            )
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                }
+            )
+            .setThumbnail(client.user.displayAvatarURL())
+            .setTimestamp()
+            .setFooter({
+                text: client.user.username,
+                iconURL: client.user.displayAvatarURL(),
+            });
+
+        channelGuild.send({ embeds: [messageEmbedded] });
 
         console.log(`Guild created successfully for ${guild.name}`);
     },
