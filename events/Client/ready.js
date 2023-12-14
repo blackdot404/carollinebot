a857db4edea9c96b8d6c5b35fba0c26ccef7c4b6
blackdot404 <blackstv11@gmail.com>
2023-12-14 13:39:38 -0300

    ✨ Ready

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -8,6 +8,6 @@ module.exports = {
         sequelize
             .sync()
             .then(() => console.log('Connection to database was successful.'));
-        console.log(`${client.user.username} is now online.`);
+        console.log(`${client.user.tag} is now online.`);
     },
 };
