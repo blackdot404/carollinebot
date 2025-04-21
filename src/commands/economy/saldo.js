e48fcbbcebfbafd82783215a6f29ec155d11c9f3
blackdot404 <blackstv11@gmail.com>
2025-04-21 16:47:58 -0300

    feat: adicionado o comando de saldo

---
+++ src/commands/economy/saldo.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('saldo')
+        .setDescription('Retorna saldo em banco do usuario especifico.')
+        .addUserOption((option) =>
+            option
+                .setName('target')
+                .setDescription('Qual usuario voce quer consultar.'),
+        ),
+    async execute(interaction, client) {
+        const selectedUser =
+            interaction.options.getUser('target') || interaction.user;
+        const storedBalance = await client.getBalance(
+            selectedUser.id,
+            interaction.guild.id,
+        );
+
+        if (!storedBalance)
+            return await interaction.reply({
+                content: `${selectedUser.tag}, usuario nao tem conta no banco.`,
+                ephemeral: true,
+            });
+        else {
+            const embed = new EmbedBuilder()
+                .setTitle(`Saldo de ${selectedUser.username}:`)
+                .setTimestamp()
+                .addFields([
+                    {
+                        name: `:coin: $${storedBalance.balance}`,
+                        value: '\u200b',
+                    },
+                ])
+                .setFooter({
+                    text: client.user.tag,
+                    iconURL: client.user.displayAvatarURL(),
+                })
+                .setColor(10944512);
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+    },
+};
