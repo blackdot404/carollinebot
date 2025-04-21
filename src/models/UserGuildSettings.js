67cae6b8ca2ac0a5a92a2ce3c36ccf1618064e33
blackdot404 <blackstv11@gmail.com>
2025-04-21 16:51:52 -0300

    refactor: mudanca na nomenclatura do schema

--- src/models/UserGuildSettings.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../database/connection');
-const { DataTypes } = require('sequelize');
-
-const UserGuildSettings = sequelize.define('UserGuildSettings', {
-    Guild: DataTypes.STRING,
-    LogChannel: DataTypes.STRING,
-});
-
-module.exports = UserGuildSettings;
