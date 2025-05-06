4254123ddbc9a78efadc98f7bb5befc2f442003e
blackdot404 <blackstv11@gmail.com>
2025-05-05 21:43:10 -0300

    refactor: mudanca no tipo de campo

--- src/models/guildBalance.js
+++ src/models/guildBalance.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize');
 const guildBalance = sequelize.define('guildBalance', {
     userId: DataTypes.STRING,
     guildId: DataTypes.STRING,
-    balance: { type: DataTypes.NUMBER, defaultValue: 0 },
+    balance: { type: DataTypes.FLOAT, defaultValue: 0.0 },
 });
 
 module.exports = guildBalance;
