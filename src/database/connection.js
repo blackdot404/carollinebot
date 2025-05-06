20b76cc6687b1884d9d532e46935feedc9b139fc
blackdot404 <blackstv11@gmail.com>
2025-05-05 21:41:35 -0300

    feat: mudanca do sqlite -> pg

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+const fs = require('fs');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false,
-});
+const sequelize = new Sequelize(
+    process.env.AIVEN_DB,
+    process.env.AIVEN_USER,
+    process.env.AIVEN_PWD,
+    {
+        host: process.env.AIVEN_HOST,
+        port: process.env.AIVEN_PORT,
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: true,
+                ca: fs.readFileSync('./ca.pem').toString(),
+            },
+        },
+    },
+);
 module.exports = sequelize;
