e291699a9b7fcf5f66ed0d978ca22d30db120f0a
Edgar Andrade <blackstv11@gmail.com>
2025-06-25 21:44:10 -0300

    fix: sistema de react sem excessao de nulo

--- src/events/guild/messageReactionAdd.js
+++ src/events/guild/messageReactionAdd.js
@@ -12,6 +12,8 @@ module.exports = {
             },
         });
 
+        if (!userGuilds) return;
+
         if (messageReaction.message.channel.id != userGuilds.channelId) {
             return;
         }
